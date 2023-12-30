f042ca31ff3dbac022fe60ca847723df31e4d0a2
Tymen <tymoteusz.zabicki@gmail.com>
2023-12-30 02:59:16 +0100

    init

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def print_hi(name):
-    print(f'Hi, {name}')
 
+# print('siemanko')
+#
+# blacks_count = int(input('ile masz czarnuchów?\n'))
+#
+# if blacks_count > 0:
+#     print("zabic")
+# elif blacks_count < 0:
+#     print("dobra liczba czarnuchow")
+# else:
+#     print("nie trzeba zabic")
+#
+# for i in range(48, 156, 4):
+#     print(i)
+# while blacks_count > 0:
+#     print(" zabic czarnuchuw 🗡️👨🏿☠️----> ", "zostalo", '👨🏿' * blacks_count, "nigerów")
+#     blacks_count -= 1
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+liczba = 1000000.12345
+dupa = f'dgsdfsdgsdfs {czarnuch:_.4f}'
+print(dupa)

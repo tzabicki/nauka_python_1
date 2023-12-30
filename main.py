473f8556b208daf104e16624085f9e3eb1137642
Tymen <tymoteusz.zabicki@gmail.com>
2023-12-30 18:43:51 +0100

    init

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # print('siemanko')
 #
 # blacks_count = int(input('ile masz czarnuchów?\n'))
@@ -16,6 +15,31 @@
 #     print(" zabic czarnuchuw 🗡️👨🏿☠️----> ", "zostalo", '👨🏿' * blacks_count, "nigerów")
 #     blacks_count -= 1
 
-liczba = 1000000.12345
-dupa = f'dgsdfsdgsdfs {czarnuch:_.4f}'
-print(dupa)
+# liczba = 1000000.12345
+# dupa = f'dgsdfsdgsdfs {czarnuch:_.4f}'
+# print(dupa)
+
+# czarnuchy = [l for l in "murzyn"]
+# print(czarnuchy)
+
+
+murzyny = [4, 5, 6, 3, 2, 2, 1, 2, 5, 5]
+for i, ocena in enumerate(murzyny):
+    if i % 3 == 0 and ocena < 6:
+        print(f"indeks: {i}, ocena:{ocena}")
+
+murzyny.append(6)
+murzyny.extend(sorted([4, 9, 11, 8],
+                      reverse=True))
+print(murzyny)
+# DOHLCV
+candle1 = [2, 3, 4, 5, 0.5]
+candle2 = [3, 4, 4, 5, 0.5]
+candle3 = [4, 5, 4, 5, 0.5]
+candle4 = [5, 6, 4, 5, 0.5]
+candles = [candle1, candle2, candle3, candle4]
+print(candles)
+# murzyny = sorted(murzyn)
+# 3 swieca high fstring
+print(f'3 swieca high= {candles[2][2]}')
+
